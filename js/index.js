6f73b2d17d0779af3c2e3efd0d78c154b4b35b71
Archanach707 <54771334+Archanach707@users.noreply.github.com>
2022-07-06 16:01:38 +0530

    1.make function for animation

--- js/index.js
+++ js/index.js
@@ -2,3 +2,20 @@ const nums = document.querySelectorAll('.nums span')
 const counter = document.querySelector('.counter')
 const finalMessage = document.querySelector('.final')
 const replay = document.querySelector('#replay')
+
+runAnimaton() 
+
+function runAnimaton() {
+  nums.forEach((num, idx) => {
+    const nextToLast = nums.length - 1
+
+    num.addEventListener('animationend', (e) => {
+      if(e.runAnimaton === 'goIn' && idx !== nextToLast) {
+        num.classList.remove('in')
+        num.classList.add('out')
+      }
+    })
+  })
+}
+
+
